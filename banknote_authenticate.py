ebdfa3b0917c55b175670e2451cac13e02f2115b
Nikhil <33477525+nikzzastic@users.noreply.github.com>
2019-09-10 19:24:07 +0530

    Update banknote_authenticate.py

--- banknote_authenticate.py
+++ banknote_authenticate.py
@@ -1,1 +1,2 @@
-# I wrote this code in Jupyter Notebook but there's some issue with github showing .ipynb files (thread already opened). So, I am changing the extension from .ipynb to .py so that if anyone wants, he/she can see the code easily
+# I wrote this code in Jupyter Notebook but there's some issue with github showing .ipynb files(thread already opened -> https://github.com/iurisegtovich/PyTherm-applied-thermodynamics/issues/11). 
+#So, I am changing the extension from .ipynb to .py so that if anyone wants, he/she can see the code easily
